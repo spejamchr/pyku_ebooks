f026a7a83f120e74072874cd9e63d38f15de1cbf
Spencer Christiansen <jc.spencer92@gmail.com>
2018-10-08 23:35:16 -0600

    Use robust path for keys file, too

--- tweet.py
+++ tweet.py
@@ -1,11 +1,14 @@
 
+import os
 import json
 import twitter
 
 from pyku_ebooks import PykuEbooks
 
+KEYS_PATH = os.path.join(os.path.dirname(__file__), "keys.json")
+
 try:
-    with open("keys.json", "r") as f:
+    with open(KEYS_PATH, "r") as f:
         keys = json.loads(f.read())
 except FileNotFoundError:
     print("""Could not find the keys.json file. Copy keys.example.json to
